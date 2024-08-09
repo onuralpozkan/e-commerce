03fb0ad11cb7bcc70d4ca019068f758df91f94ea
onuralp.ozkan <onuralp.ozkan@dk.hepsipay.com>
2024-08-10 02:50:53 +0300

    categories section edit

--- src/features/Categories/containers/Categories.template.tsx
+++ src/features/Categories/containers/Categories.template.tsx
@@ -5,45 +5,41 @@ import Button from "~/common/components/button/Button";
 const CategoriesTemplate = ({ data }: { data: Category[] }) => {
   const CATEGORIES_PREVIEW_COUNT = 3;
   const [showCategories, setShowCategories] = useState(false);
+
   const onClick = () => {
+    setShowCategories(!showCategories);
     setCount((prevCount) =>
       prevCount === data.length ? CATEGORIES_PREVIEW_COUNT : data.length
     );
   };
-  const [count, setCount] = useState(3);
+  const [count, setCount] = useState(CATEGORIES_PREVIEW_COUNT);
 
-  const hasMoreCategories = data.length > CATEGORIES_PREVIEW_COUNT;
-  const dataPreview = hasMoreCategories ? data.slice(0, count) : data;
-
-  const list = dataPreview.map((category) => (
+  const list = data.slice(0, count).map((category) => (
     <li className="categories__list-item" key={category.id}>
       <Button
         onClick={() => {
           console.log(category);
           setShowCategories(false);
         }}
-        variant="secondary"
+        variant="text"
       >
         {category.name}
       </Button>
     </li>
   ));
 
+  const text = `${
+    data.length > CATEGORIES_PREVIEW_COUNT && showCategories ? "Hide" : "Show"
+  } Categories`;
+
   return (
     <nav className="categories">
-      <Button className="categories__button" variant="text">
-        Categories
+      <Button className="categories__button" variant="text" onClick={onClick}>
+        {text}
       </Button>
-      <ul className="categories__list">{list}</ul>
-      {hasMoreCategories && (
-        <Button
-          className="categories__show-more-button"
-          variant="text"
-          onClick={onClick}
-        >
-          Show more
-        </Button>
-      )}
+      <ul className={`categories__list ${showCategories ? "open" : "close"}`}>
+        {list}
+      </ul>
     </nav>
   );
 };
